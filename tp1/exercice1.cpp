cde8c35fec93708c5b666cf15843ab09d3fef74d
tom <tom.martzolf@etu.unilim.fr>
2019-09-18 20:02:56 +0200

    exercice 1 tp1

--- tp1/exercice1.cpp
+++ tp1/exercice1.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int sommePremiers(int n){
+  int somme=0;
+  for(int i =0; i<n ;i++){
+    somme=somme+i;
+  }
+  return somme;
+}
+
 int main(){
-        cout << "Hello world!"<<endl;
-        cout << "oui!"<<endl;
-      system("PAUSE");
+
+      int n = 0 ;
+
+      cout<< "saisir valeur de n :";
+      cin>>n;
+      cout<< endl << "somme des " << n <<" premiers entiers : " << sommePremiers(n);
 return 0;
 }
- 
