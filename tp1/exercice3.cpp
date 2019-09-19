cfa60bfce4f39a3778dae38a4a427e2f328e5c8d
Dorian <dorianmare@yahoo.fr>
2019-09-19 11:04:25 +0200

    exo1 et exo3

---
+++ tp1/exercice3.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+///////////////////////////
+///  fonction controleSup qui permet de saisir
+///   un entier positif et de le controler
+///
+///  entrée : rien
+///  sortie : un entier positif
+/////////////////////////
+int controleSup(){
+  int n = 0;
+  cin>>n;
+  while(n<0){
+    cout<<"erreur entier negatif re-saisir valeur de n :";
+    cin>>n;
+  }
+  return n;
+}
+
+//////////////////////////////////////////////////////////////////
+//Description : retourne le PGCD des deux entiers pris en entrees
+//
+//Entrees : deux entiers quelconque
+//
+//Sorties : le PGCD de deux entiers
+//
+//Pre-Conditions : les entiers en entrees ne sont pas negatif
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+int PGCD(int a, int b){
+  if (a>b) {
+    PGCD(a-b, b);
+  } else if (a<b){
+    PGCD(a, b-a);
+  }
+  else {
+    return a;
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+//Description : programme principal ou l'utilisateur rentre ses deux entiers
+//
+//Entrees : rien
+//
+//Sorties : rien
+//
+//Pre-Conditions : Aucune
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+int main(){
+  int a, b, resultat;
+  a = 0;
+  b = 0;
+
+  cout << "Rentrer le premier chiffre : ";
+  a = controleSup();
+  cout << "Rentrer le second chiffre : ";
+  b = controleSup();
+
+  resultat = PGCD(a,b);
+  cout << "voici le PGCD de " << a << " et " << b << " : " << resultat;
+  return 0;
+}
+
+/*
+condition d'arret :
+Si a = b on retourne a ou b
+Relation de récurrence :
+PGCD(a,b) = PGCD(a-b, b) si a>b sinon si a<b PGCD(a,b) = PGCD(a, b-a)
+*/
