e665f2ad19265debe3c6435db6398a7e3683d6c1
Dorian <dorianmare@yahoo.fr>
2019-09-19 11:07:17 +0200

    exo1

--- tp1/exercice1.cpp
+++ tp1/exercice1.cpp
@@ -1,20 +1,63 @@
 #include <iostream>
 using namespace std;
 
-int sommePremiers(int n){
-  int somme=0;
-  for(int i =0; i<n ;i++){
-    somme=somme+i;
+///////////////////////////
+///  fonction controleSup qui permet de saisir
+///   un entier positif et de le controler
+///
+///  entrée : rien
+///  sortie : un entier positif
+/////////////////////////
+int controleSup(){
+  int n = 0;
+  cin>>n;
+  while(n<0){
+    cout<<"erreur entier negatif re-saisir valeur de n :";
+    cin>>n;
   }
-  return somme;
+  return n;
 }
 
-int main(){
+//////////////////////////////////////////////////////////////////
+//Description : retourne la somme des premiers entiers pour un certain chiffre
+//
+//Entrees : le nombre d'entiers premiers a additionner
+//
+//Sorties : la somme des premiers entiers
+//
+//Pre-Conditions : l'entier en entree ne doit pas etre negatif
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+int sommePremiers(int n){
+  if (n > 0) {
+    return n + sommePremiers(n - 1);
+  }
+}
 
+//////////////////////////////////////////////////////////////////
+//Description : programme principal ou l'utilisateur rentre un entier
+//
+//Entrees : rien
+//
+//Sorties : rien
+//
+//Pre-Conditions : Aucune
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+int main(){
       int n = 0 ;
 
-      cout<< "saisir valeur de n :";
-      cin>>n;
+      cout << "Rentrer un entier : ";
+      n = controleSup();
       cout<< endl << "somme des " << n <<" premiers entiers : " << sommePremiers(n);
 return 0;
 }
+
+/*
+Condition d'arret :
+Quand n <= 0
+Recursivité :
+sommePremiers(n) = n + sommePremiers(n-1) tant que n>0
+*/
