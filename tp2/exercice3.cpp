c9ecee5286659bc94d845de5fcacf8e2ea94f76a
tom <tom.martzolf@etu.unilim.fr>
2019-09-27 10:16:27 +0200

    tp 2 exo 3

---
+++ tp2/exercice3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void TriRapide( int tableau[], int a, int b ){
+  int pivot;
+  int i,j ; // pour parcourir le tableau
+  int ip, jp; // pour l'écgande de valeurs
+  int temp; // variable tampon pour l'échange
+
+  if(a<b){
+
+    i=a;
+    j=b;
+    x=tableau[(a+b)/2];
+  }
+
+
+}
