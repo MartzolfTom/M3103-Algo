7ff42b68f43f5392e0b65deb839d40f12403025b
tom <tom.martzolf@etu.unilim.fr>
2019-09-27 11:51:16 +0200

    exo 3 fuck merillou

--- tp2/exercice3.cpp
+++ tp2/exercice3.cpp
@@ -8,11 +8,49 @@ void TriRapide( int tableau[], int a, int b ){
   int temp; // variable tampon pour l'échange
 
   if(a<b){
-
     i=a;
     j=b;
-    x=tableau[(a+b)/2];
+    pivot=tableau[((a+b)/2)];
+    while(i<=j){
+      ip=i;
+      while(tableau[ip]<pivot){
+        ip=ip+1;
+      }
+      jp=j;
+      while(tableau[jp]> pivot){
+        jp=jp-1;
+      }
+      if(ip<=jp){
+        temp=tableau[ip];
+        tableau[ip]=tableau[jp];
+        tableau[jp]=temp;
+        i=ip+1;
+        j=jp-1;
+      }
+      else{
+        i=ip;
+        j=jp;
+      }
+    }
+	TriRapide(tableau, a, j);
+	TriRapide(tableau, i, b);
   }
+}
 
+
+int main(){
+
+	int tableau[] = { 5,8,46,95,28,3,4,75,9,10 };
+  cout<<"tableau sans tri : ";
+  for(int i=0;i<10;i++){
+    cout<<tableau[i]<<" ";
+  }
+  cout<<'\n';
+TriRapide(tableau,0,9);
+
+  cout<<"tableau avec tri : ";
+for(int i=0;i<10;i++){
+  cout<<tableau[i]<<" ";
+}
 
 }
