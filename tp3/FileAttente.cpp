9f103c4dca1f7f0d88cf78619d164d960860c7ed
tom <tom.martzolf@etu.unilim.fr>
2019-10-09 08:17:00 +0200

    capsules et ajout commentaires

--- tp3/FileAttente.cpp
+++ tp3/FileAttente.cpp
@@ -2,15 +2,40 @@
 #include "FileAttente.hpp"
 using namespace std;
 
+//////////////////////////////////////////////////////////////////
+//Description : Fonction qui alloue la memoire d'une file d'attente
+//
+//Entrees : rien
+//
+//Sorties : retourne une variable de type FileAttente vide
+//
+//Pre-Conditions : aucune
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+
 FileAttente fileNouv()
 {
   FileAttente file;
 
-  file.tete = nullptr;
+  file.tete = nullptr;    // la file ne contient aucuns maillons, on initialise donc à 'null'
   file.queue = nullptr;
 
   return file;
 }
+
+//////////////////////////////////////////////////////////////////
+//Description : procédure ajoutant un maillon contenant un tableau de caractère
+//              à la fille d'attente en queue
+//
+//Entrees : variable de type FileAttente et tableau de caractère
+//
+//Sorties : variable de type FileAttente
+//
+//Pre-Conditions : le taleau de caractère ne doit pas excéder
+//                 caractères
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
 
 void ajouterQueue(FileAttente& file, char personne[20])
 {
@@ -40,9 +65,22 @@ void ajouterQueue(FileAttente& file, char personne[20])
   file.queue = nouvMaillon;
 }
 
+
+//////////////////////////////////////////////////////////////////
+//Description : procédure retirant le maillon en tête d'une fille d'attente
+//
+//Entrees : variable de type FileAttente
+//
+//Sorties : variable de type FileAttente
+//
+//Pre-Conditions :
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+
 void retirerTete(FileAttente &file)
 {
-  if (file.tete == nullptr)
+  if (file.tete == nullptr)    // cas où la file ne contient aucun maillons
   {
     cout << "Il n'y a rien a supprimmer." << endl;
   }
@@ -54,23 +92,48 @@ void retirerTete(FileAttente &file)
     file.tete = buffer;
   }
 
-  //si jamais la longueur ed la chaine est de 0 ou 1, alors la queue est la meme que la tete
+  //si jamais la longueur ed la chaine est de 0 ou 1, alors la queue est la même que la tete
   if (longueurFile(file) <= 1) {
     file.queue = file.tete;
   }
 }
 
+//////////////////////////////////////////////////////////////////
+//Description : procédure permettant de consulter le contenu
+//              du maillon en tête de la file d'attente
+//
+//Entrees : variable de type FileAttente et tableau de caractère
+//
+//Sorties : variable de type FileAttente
+//
+//Pre-Conditions : le taleau de caractère ne doit pas excéder
+//                 caractères
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
+
 void ConsulterTeteFile(FileAttente file)
 {
-  if (file.tete == nullptr)
+  if (file.tete == nullptr)     // cas où la file ne contient aucun maillons
   {
     cout << "La file est vide." << endl;
   }
   else
-  {
+  {         // affichage du contenu du maillon en tête de la file
     cout << "Valeur de la tete de file : " << (*file.tete).personne << endl;
   }
 }
+
+//////////////////////////////////////////////////////////////////
+//Description : fonction retournant la nombre de maillon dans la file d'attente
+//
+//Entrees : variable de type FileAttente
+//
+//Sorties : entier ( nombre de maillon de la file)
+//
+//Pre-Conditions :
+//
+//Post-Condition : aucune
+//////////////////////////////////////////////////////////////////
 
 int longueurFile(FileAttente file)
 {
@@ -78,7 +141,7 @@ int longueurFile(FileAttente file)
   int longueur = 0;
   maillon = file.tete;
 
-  while (maillon != nullptr)
+  while (maillon != nullptr)   // boucle pour incrémenter le compteur du nombre de maillon
   {
     maillon = (*maillon).suivant;
     longueur++;
